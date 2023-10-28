e5993fe38eefd6efe435bbb045e968e970c9d4ad
intellect-sam <alukoolakunlesamuel@gmail.com>
2023-10-28 10:39:41 +0100

    packages

--- src/Second.java
+++ src/Second.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
 class Second {
     public static void main(String[] args) {
         // Main myCar = new Main();
         // myCar.fullThrottle();
         // myCar.speed(200);
 
+        Person myPerson = new Person();
+        myPerson.setName("John");
+        System.out.println(myPerson.getName());
+
         // Calling the function from a previous file 
-        Main.getName("Samuel");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Name:");
+
+        String userName = myObj.nextLine();
+        Main.getName(userName);
+    
 
         // This is a constructor
         Main myCar = new Main(1969, "Corolla");
-        System.out.println(myCar.modelYear + " " + myCar.modelName );
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
+        
     }
-
-    
 }
