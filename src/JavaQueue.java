235447f0db4277cd7e06bee4cd7ae4f14f3dd2ed
intellect-sam <alukoolakunlesamuel@gmail.com>
2023-11-07 22:08:43 +0100

    JavaTreeMap

---
+++ src/JavaQueue.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaQueue {
+
+    public static void main(String[] args) {
+        Queue<String> waitingQueue = new LinkedList<>();
+
+         waitingQueue.add("Rajeev");
+        waitingQueue.add("Chris");
+        waitingQueue.add("John");
+        waitingQueue.add("Mark");
+        waitingQueue.add("Steven");
+    }
+    
+}
